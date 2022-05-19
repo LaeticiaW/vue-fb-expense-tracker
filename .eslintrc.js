775158334cc73f36f8354a21bf9693c3320cba44
Laeticia Warring <clmlkw@Laeticias-MacBook-Pro.local>
2022-05-19 14:30:32 -0400

    clean up

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: ['plugin:vue/essential', 'plugin:prettier/recommended', 'eslint:recommended'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? ['error', { allow: ['info', 'warn', 'error'] }] : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     semi: [2, 'never'],
     'comma-dangle': 'off',
